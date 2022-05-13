0423bb13e3cab3b90dfd18a1f214dd4d37569bf1
secundant <mysecundant@gmail.com>
2022-05-14 01:31:25 +0300

    feat(generator): create react hook generator

---
+++ libs/generator/templates/hook/new/prompt.js
@@ -0,0 +1,33 @@
+const { prompts } = require('../../../internal');
+
+module.exports = {
+  async prompt({ prompter, args, h }) {
+    const { prompt, getAnswers } = prompts(prompter, {
+      defaults: args,
+      silent: args.silent
+    });
+
+    await prompt([
+      {
+        name: 'rootDir',
+        type: 'input',
+        silent: true,
+        message: 'Path to hooks root folder'
+      },
+      {
+        name: 'name',
+        type: 'input',
+        message: 'Hook name',
+        format: value => `use-${value}`,
+        result: raw => {
+          const name = h.changeCase.kebab(raw);
+
+          return name.startsWith('use') ? name : `use-${name}`;
+        },
+        validate: value => value.length > 2 || 'Name length should be greater than 2'
+      }
+    ]);
+
+    return getAnswers();
+  }
+};
